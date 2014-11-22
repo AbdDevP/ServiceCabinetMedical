e8eaec2246c6efb03d9186d28bb57cff42bd9d55
abdennourmiage <abdennourboutrig@gmail.com>
2014-11-22 10:53:38 +0100

    ConsultationService

--- gestioncabinet-coreM/src/miage/gestioncabinet/coreM/ConsultationService.java
+++ gestioncabinet-coreM/src/miage/gestioncabinet/coreM/ConsultationService.java
@@ -1,44 +1,57 @@
 package miage.gestioncabinet.coreM;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+
+import javax.ejb.Stateful;
 
 import miage.gestioncabinet.api.Consultation;
 import miage.gestioncabinet.api.ConsultationRemoteService;
 import miage.gestioncabinet.api.GestionCabinetException;
 import miage.gestioncabinet.api.Produit;
+import miage.gestioncabinet.api.Traitement;
 
-public class ConsultationRemoteServiceImpl implements ConsultationRemoteService{
+@Stateful
+public class ConsultationService implements ConsultationRemoteService {
+
+	private Consultation consultation;
 
 	@Override
 	public Consultation getConsultation() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.consultation;
 	}
 
 	@Override
 	public void setConsultation(Consultation consultation) {
-		// TODO Auto-generated method stub
-		
+		this.consultation = consultation;
+
 	}
 
 	@Override
 	public Consultation creerRdv(Calendar date) {
-		// TODO Auto-generated method stub
-		return null;
+		this.consultation = (Consultation) new ConsultationImpl(date);
+		return this.consultation;
 	}
 
 	@Override
 	public List<Produit> rechercherMedicament(String keyword)
 			throws GestionCabinetException {
-		// TODO Auto-generated method stub
-		return null;
+		List<Traitement> traitements  = this.consultation.getPrescription();
+		List<Produit> listeProduits = new ArrayList<Produit>();
+		for (Traitement traitement : traitements) {
+			if(traitement.getProduit().getNom().equals(keyword)){
+				listeProduits.add(traitement.getProduit());
+			}
+		}
+		return listeProduits;
+		
 	}
 
 	@Override
 	public void analyserPrescription() throws GestionCabinetException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
@@ -50,7 +63,7 @@ public class ConsultationRemoteServiceImpl implements ConsultationRemoteService{
 	@Override
 	public void supprimer() throws GestionCabinetException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
